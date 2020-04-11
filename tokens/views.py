716ac7a2c587617a4a7eb5715abef2a370563105
Brian Yu <brianyu28@gmail.com>
2020-04-11 17:45:49 -0400

    Document view

--- tokens/views.py
+++ tokens/views.py
@@ -8,6 +8,8 @@ def index(request):
     return render(request, "tokens/index.html")
 
 def claim(request):
+
+    # Validate email and one-time password
     email = request.POST.get("email", "")
     password = request.POST.get("password", "")
     try:
@@ -17,18 +19,22 @@ def claim(request):
             "error": "Invalid email address and/or one-time-password."
         })
 
+    # Ensure each voter can only claim a single token
     if voter.claimed:
         return render(request, "tokens/index.html", {
             "error": "Token has already been claimed."
         })
 
+    # Generate a new token for the user
     token = Token(token=uuid.uuid4().hex)
     token.save()
 
+    # Record that voter has claimed a token
     voter.election.tokens.add(token)
     voter.claimed = True
     voter.save()
 
+    # Display token
     return render(request, "tokens/claimed.html", {
         "token": token
     })
