452b12bff5890444cee09c7c5b50ce3bf66a7335
Brian Yu <brianyu28@gmail.com>
2020-04-16 14:41:31 -0400

    Use different secret key in production

--- votetoken/settings.py
+++ votetoken/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ykvhe@u_9hd8%^@5-o-j)orh&1ak%z8-bm%#_#p(8gby+pk&hr'
+SECRET_KEY = os.environ.get("SECRET_KEY", 'ykvhe@u_9hd8%^@5-o-j)orh&1ak%z8-bm%#_#p(8gby+pk&hr')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
